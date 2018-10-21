f9bb3eb2f1e07b3582157823e67349ec2aaf834b
antmarr <test@test.ru>
2018-10-21 21:24:34 +0400

    DASH-153 add chart widget

---
+++ modules/web/src/com/haulmont/addon/dashboardchart/web/widget/ChartWidget.java
@@ -0,0 +1,121 @@
+package com.haulmont.addon.dashboardchart.web.widget;
+
+import com.haulmont.addon.dashboard.gui.components.DashboardFrame;
+import com.haulmont.addon.dashboard.model.Dashboard;
+import com.haulmont.addon.dashboard.model.Parameter;
+import com.haulmont.addon.dashboard.model.Widget;
+import com.haulmont.addon.dashboard.web.annotation.DashboardWidget;
+import com.haulmont.addon.dashboard.web.annotation.WidgetParam;
+import com.haulmont.addon.dashboard.web.parametertransformer.ParameterTransformer;
+import com.haulmont.bali.util.ParamsMap;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.gui.WindowParam;
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.GroupBoxLayout;
+import com.haulmont.cuba.gui.components.Label;
+import com.haulmont.reports.entity.Report;
+import com.haulmont.reports.entity.ReportOutputType;
+import com.haulmont.reports.entity.ReportTemplate;
+import com.haulmont.reports.gui.ReportGuiManager;
+import com.haulmont.yarg.reporting.ReportOutputDocument;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static com.haulmont.addon.dashboardchart.web.widget.ChartWidget.CAPTION;
+
+
+@DashboardWidget(name = CAPTION, editFrameId = "dashboardchart$ChartWidget.edit")
+public class ChartWidget extends AbstractFrame {
+
+    public static final String CAPTION = "Chart";
+
+    private final String CHART_JSON_PARAMETER = "chartJson";
+    private final String JSON_CHART_SCREEN_ID = "chart$jsonChart";
+
+    @Inject
+    protected DataManager dataManager;
+
+    @Inject
+    protected ReportGuiManager reportGuiManager;
+
+    @Inject
+    private ParameterTransformer parameterTransformer;
+
+    @WindowParam
+    protected Widget widget;
+
+    @WindowParam
+    protected Dashboard dashboard;
+
+    @WindowParam
+    protected DashboardFrame dashboardFrame;
+
+    @WidgetParam
+    @WindowParam
+    protected UUID reportId;
+
+    @WidgetParam
+    @WindowParam
+    protected UUID templateId;
+
+    @Named("chartBox")
+    protected GroupBoxLayout chartBox;
+
+    @Named("errorLabel")
+    protected Label errorLabel;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+
+        LoadContext<Report> loadContext = LoadContext.create(Report.class)
+                .setId(reportId)
+                .setView("report.edit");
+
+        Report report = dataManager.load(loadContext);
+        ReportTemplate reportTemplate = null;
+        if (report != null) {
+            List<ReportTemplate> chartTemplates = report.getTemplates().stream().filter(rt -> ReportOutputType.CHART == rt.getReportOutputType()).collect(Collectors.toList());
+            if (templateId != null) {
+                reportTemplate = chartTemplates.stream()
+                        .filter(t -> templateId.equals(t.getId())).findFirst()
+                        .orElse(ReportOutputType.CHART == report.getDefaultTemplate().getReportOutputType() ? report.getDefaultTemplate() : null);
+            }
+
+        }
+        if (report == null || reportTemplate == null) {
+            errorLabel.setVisible(true);
+            chartBox.setVisible(false);
+            return;
+        }
+
+        Map<String, Object> widgetParams = new HashMap<>();
+        for (Parameter p : widget.getParameters()) {
+            Object rawValue = parameterTransformer.transform(p.getParameterValue());
+            widgetParams.put(p.getName(), rawValue);
+        }
+        ReportOutputDocument document = reportGuiManager.getReportResult(report, widgetParams, reportTemplate.getCode());
+
+        if (document.getContent() != null) {
+            try {
+                chartBox.removeAll();
+                openFrame(chartBox, JSON_CHART_SCREEN_ID, ParamsMap.of(CHART_JSON_PARAMETER, new String(document.getContent(), "UTF-8")));
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            errorLabel.setVisible(true);
+            chartBox.setVisible(false);
+        }
+    }
+
+
+}
---
+++ modules/web/src/com/haulmont/addon/dashboardchart/web/widget/ChartWidgetEdit.java
@@ -0,0 +1,86 @@
+package com.haulmont.addon.dashboardchart.web.widget;
+
+import com.haulmont.addon.dashboard.web.annotation.WidgetParam;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.gui.WindowParam;
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.LookupField;
+import com.haulmont.reports.entity.Report;
+import com.haulmont.reports.entity.ReportOutputType;
+import com.haulmont.reports.entity.ReportTemplate;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class ChartWidgetEdit extends AbstractFrame {
+
+    @Inject
+    protected DataManager dataManager;
+
+    @Named("reportLookup")
+    protected LookupField reportLookup;
+
+    @Named("templateLookup")
+    protected LookupField templateLookup;
+
+    @WidgetParam
+    @WindowParam
+    protected UUID reportId;
+
+    @WidgetParam
+    @WindowParam
+    protected UUID templateId;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+
+        reportLookup.addValueChangeListener(r -> {
+            Report report = (Report) r.getValue();
+            if (report != null) {
+                List<ReportTemplate> chartTemplates = report.getTemplates().stream().filter(rt -> ReportOutputType.CHART == rt.getReportOutputType()).collect(Collectors.toList());
+                templateLookup.setOptionsList(chartTemplates);
+                reportId = report.getId();
+                if (ReportOutputType.CHART == report.getDefaultTemplate().getReportOutputType()) {
+                    templateLookup.setValue(report.getDefaultTemplate());
+                    templateId = report.getDefaultTemplate().getId();
+                }
+            }
+
+        });
+
+        templateLookup.addValueChangeListener(t -> {
+            ReportTemplate reportTemplate = (ReportTemplate) t.getValue();
+            if (reportTemplate != null) {
+                templateId = reportTemplate.getId();
+            }
+        });
+
+        if (reportId != null) {
+            LoadContext<Report> loadContext = LoadContext.create(Report.class)
+                    .setId(reportId)
+                    .setView("report.edit");
+
+            Report report = dataManager.load(loadContext);
+            if (report != null) {
+                List<ReportTemplate> chartTemplates = report.getTemplates().stream().filter(rt -> ReportOutputType.CHART == rt.getReportOutputType()).collect(Collectors.toList());
+                ReportTemplate reportTemplate = null;
+                if (templateId != null) {
+                    reportTemplate = chartTemplates.stream()
+                            .filter(t -> templateId.equals(t.getId())).findFirst()
+                            .orElse(ReportOutputType.CHART == report.getDefaultTemplate().getReportOutputType() ? report.getDefaultTemplate() : null);
+                }
+                reportLookup.setValue(report);
+                templateLookup.setOptionsList(chartTemplates);
+                templateLookup.setValue(reportTemplate);
+            }
+        }
+    }
+
+
+}
