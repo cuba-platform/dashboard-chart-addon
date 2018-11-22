4ae385a5e53305e54ad271a688d0f10135b1423b
Evgeny Zaharchenko <zaharchenko@haulmont.com>
2018-11-22 10:07:41 +0400

    DASH-152 Create component documentation
    dashboard chart addon documentation

---
+++ modules/core/src/com/haulmont/addon/dashboardchart/core/DashboardChartBean.java
@@ -0,0 +1,9 @@
+package com.haulmont.addon.dashboardchart.core;
+
+import org.springframework.stereotype.Component;
+
+@Component("dashboardchart_DashboardChart")
+public class DashboardChartBean {
+
+
+}
